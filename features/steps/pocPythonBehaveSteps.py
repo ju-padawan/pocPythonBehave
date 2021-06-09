a6b3fe9679850aa3c623885cc95724a1c644a94f
Juliana Albuquerque de Oliveira <CIANDT\julianaalbuquerque@lnb028742cps.ciandt.global>
2021-06-09 15:32:28 -0300

    Aplicando correçoes nas configuraçoes de debug

--- features/steps/pocPythonBehaveSteps.py
+++ features/steps/pocPythonBehaveSteps.py
@@ -6,6 +6,7 @@ import time
 def step_impl(context):
     context.driver.get('https://automacaocombatista.herokuapp.com/treinamento/home')
     context.driver.maximize_window()
+    context.driver.save_screenshot("results/passo001.png")
 
 
 @when(u'acessar mudança de foco para acessar uma nova janela')
@@ -13,6 +14,7 @@ def step_impl(context):
     context.driver.find_element(By.XPATH, "//*[contains(text(), 'Mudança de foco')]").click()
     time.sleep(2)
     context.driver.find_element(By.XPATH, "//*[contains(text(), 'Janela')]").click()
+    context.driver.save_screenshot("results/passo002.png")
     context.driver.find_element(By.XPATH, "//*[contains(text(), 'Clique aqui')]").click()
 
 
@@ -22,3 +24,4 @@ def step_impl(context):
     context.driver.switch_to.window(context.driver.window_handles[1])
     elemento = str(context.driver.current_url)
     assert "https://automacaocombatista.herokuapp.com/mudancadefoco/newwindow" in str(elemento)
+    context.driver.save_screenshot("results/passo003.png")
